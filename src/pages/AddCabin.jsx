2c6466bca170e4a84af14636b9da4718a1e53301
Cuong NQ1 <cuongnq1@bsscommerce.com>
2025-06-23 01:30:22 +0700

    new commit

--- src/pages/AddCabin.jsx
+++ src/pages/AddCabin.jsx
@@ -2,40 +2,18 @@ import Button from '../ui/Button';
 import CreateCabinForm from '../features/cabins/CreateCabinForm';
 import Modal from '../ui/Modal';
 
-// function AddCabin() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
-
-//   return (
-//     <div>
-//       <Button onClick={() => setIsOpenModal((open) => !open)}>
-//         Add new cabin
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
-
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens='cabin-form'>
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name='cabin-form'>
-        <CreateCabinForm />
-      </Modal.Window>
-
-      {/* <Modal.Open>
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window>
-        <CreateCabinForm />
-      </Modal.Window> */}
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens='cabin-form'>
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name='cabin-form'>
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
