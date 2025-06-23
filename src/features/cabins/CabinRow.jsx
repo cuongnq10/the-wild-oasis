f5a962614c60632aa4c2188d8400cd65586cdad9
Cuong NQ1 <cuongnq1@bsscommerce.com>
2025-06-23 21:12:27 +0700

    new commit

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -7,18 +7,8 @@ import { HiPencil, HiSquare2Stack, HiTrash } from 'react-icons/hi2';
 import { useCreateCabin } from './useCreateCabin';
 import Modal from '../../ui/Modal';
 import ConfirmDelete from '../../ui/ConfirmDelete';
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+import Table from '../../ui/Table';
+import Menus from '../../ui/Menus';
 
 const Img = styled.img`
   display: block;
@@ -72,7 +62,7 @@ function CabinRow({ cabin }) {
   }
 
   return (
-    <TableRow role='row'>
+    <Table.Row role='row'>
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
@@ -95,12 +85,12 @@ function CabinRow({ cabin }) {
           <Modal.Window name='edit'>
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
-          <Modal.Open opens='confirmDelete'>
+          <Modal.Open opens='delete'>
             <button>
               <HiTrash />
             </button>
           </Modal.Open>
-          <Modal.Window name='confirmDelete'>
+          <Modal.Window name='delete'>
             <ConfirmDelete
               resourceName='cabins'
               disabled={isDeleting}
@@ -108,8 +98,16 @@ function CabinRow({ cabin }) {
             />
           </Modal.Window>
         </Modal>
+        <Menus.Menu>
+          <Menus.Toggle id={cabinId} />
+          <Menus.List id={cabinId}>
+            <Menus.Button>Duplicate</Menus.Button>
+            <Menus.Button>Edit</Menus.Button>
+            <Menus.Button>Delete</Menus.Button>
+          </Menus.List>
+        </Menus.Menu>
       </div>
-    </TableRow>
+    </Table.Row>
   );
 }
 
